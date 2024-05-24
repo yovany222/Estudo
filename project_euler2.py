b7d3609131943e7d1a2908fa788d51d0aa556d67
Yovany <yovany.argolo@gmail.com>
2024-05-24 13:21:59 -0300

    novo

---
+++ project_euler2.py
@@ -0,0 +1,16 @@
+primeiro_numero = 0
+segundo_numero = 1
+terceiro_numero = 0
+soma = 0
+
+while primeiro_numero < 4000000:
+
+    print(primeiro_numero)
+    terceiro_numero = primeiro_numero + segundo_numero
+    primeiro_numero = segundo_numero
+    segundo_numero = terceiro_numero
+    
+    if primeiro_numero % 2 == 0:
+        soma = soma + primeiro_numero
+
+print(f"A soma dos pares é: {soma}")
