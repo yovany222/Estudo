6aa6ef80333dc48caa513ce7134fc3e64bc57fa1
Yovany <159599689+yovany222@users.noreply.github.com>
2024-07-04 12:12:06 -0300

    Update and rename teste to project_euler3.py

---
+++ project_euler3.py
@@ -0,0 +1,13 @@
+def fatores_primos(n):
+
+    while n % 2 == 0 and n != 2 :
+        n = n/2
+    i = 3
+    while i < n ** 0.5:
+        if n % i == 0:
+            n = n / i
+        i = i + 2
+    return n 
+
+            
+print(int(fatores_primos(600851475143)))
