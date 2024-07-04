91024c14e54cb2634df466069f46d752e9853199
Yovany <159599689+yovany222@users.noreply.github.com>
2024-07-04 12:57:23 -0300

    Create project_euler4.py

---
+++ project_euler4.py
@@ -0,0 +1,13 @@
+def eh_palindromo(n):
+    return str(n) == str(n)[::-1]
+
+def maior_palindromo():
+    maior_pal = 0
+    for i in range(999, 99, -1): 
+        for j in range(i, 99, -1): 
+            produto = i * j
+            if eh_palindromo(produto) and produto > maior_pal:
+                maior_pal = produto
+    return maior_pal
+
+print(maior_palindromo())
